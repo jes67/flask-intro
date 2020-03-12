9d439e024d88db9c23c93d03ba3fa06ce34f8907
jesusarreb <jesusmlg16@gmail.com>
2020-03-12 19:46:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run()
